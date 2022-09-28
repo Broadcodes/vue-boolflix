dd2190a0fd9a81e6c3eeb4065fb5b18c3b4dc472
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-09-28 16:54:30 +0200

    Milestone 1: Inserimento input e button nell'header

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,32 @@
 <template>
   <div id="app">
+    <header>
+      <HeaderComponent />
+    </header>
+    
+    <main>
+      <HomepageContainer />
 
+    </main>
   </div>
 </template>
 
 <script>
+  import HeaderComponent from './components/headerComponent.vue';
+  import HomepageContainer from './components/homepageContainer.vue';
 
 
 export default {
   name: 'App',
   components: {
-
-  }
+    HeaderComponent,
+    HomepageContainer,
+}
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-
+ 
 }
 </style>
---
+++ src/components/homepageContainer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- Qui verranno inseriti tutti quegli elementi che sono presenti nella schermata home
+    prima di effettuare la ricerca.
+    Per intenderci la pagina iniziale presente su netflix originale -->
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

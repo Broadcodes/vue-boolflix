75ebf13f07f5c84dc9b251fafa2b21e4a3e9eaf0
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-09-28 21:34:52 +0200

    Milestone 2: Trasformazione della stringa statica della lingua in una bandiera della nazione corrispondente

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -3,7 +3,7 @@
     <div v-for="item in dataMoviesListObj" :key="item.id">
       <p>Titolo: {{ item.title }}</p>
       <p>Titolo Originale: {{ item.original_title }}</p>
-      <p>Lingua: {{ item.original_language }}</p>
+      <img :src="setFlagIcon(item.original_language)" :alt="item.original_language" @error="replaceFlagNotFound">
       <p>Voto: {{ item.vote_average }}</p>
       <p>------------------------------------------------------------------------</p>
     </div>
@@ -11,9 +11,21 @@
 </template>
   
   <script>
+import errorFlagIcon from "@/assets/ErrorFlag/world.png";
+
 export default {
   data() {
-    return {};
+    return {
+        
+    };
+  },
+  methods:{
+    setFlagIcon(country){
+        return "https://www.countryflagicons.com/FLAT/32/" + country.toUpperCase() + ".png";
+    },
+    replaceFlagNotFound(e){
+        e.target.src = errorFlagIcon;
+    }
   },
   props: {
     dataMoviesListObj: Array,
--- src/components/headerComponent.vue
+++ src/components/headerComponent.vue
@@ -1,14 +1,3 @@
-<!-- Milestone 1:
-Creare un layout base con una searchbar (un input e un button) in cui possiamo
-scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il
-bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
-Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni
-film trovato:
-1. Titolo
-2. Titolo Originale
-3. Lingua
-4. Voto -->
-
 <template>
   <header>
     <input type="text" v-model="searchMoviesText" @keyup.enter="searchUserMovies"/>

4ff1f9789313f5e8081f70b617ad7d7240d350f5
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-09-29 23:07:02 +0200

    Milestone 4: Creazione dell'header

--- src/components/headerComponent.vue
+++ src/components/headerComponent.vue
@@ -1,7 +1,30 @@
 <template>
   <header>
-    <input type="text" v-model="searchMoviesText" @keyup.enter="searchUserMovies"/>
-    <button @click="searchUserMovies">Cerca</button>
+    <img class="logo" src="../assets/img/boolflix_logo.png" alt="Boolflix">
+    <div class="navigationBar">
+      <div class="navSx">
+        <ul>
+          <li>Home</li>
+          <li>Serie</li>
+          <li>Film</li>
+          <li>Aggiunti di recente</li>
+          <li>La mia lista</li>
+        </ul>
+      </div>
+      <div class="navDx">
+        <ul>
+          <li><i class="fa-solid fa-magnifying-glass" @click="viewSearch"></i></li>
+          <li><i class="fa-solid fa-bell"></i></li>
+          <li><img src="https://picsum.photos/200/300?random=0" alt="My Profile"></li>
+        </ul>
+        <div class="searchMoviesAndSeries">
+          <input type="text" v-model="searchMoviesText" @keyup.enter="searchUserMovies" />
+          <button @click="searchUserMovies">Cerca</button>
+        </div>
+      </div>
+    </div>
+
+
   </header>
 </template>
 
@@ -9,19 +32,126 @@
 export default {
   data() {
     return {
-        searchMoviesText: '',
+      searchMoviesText: '',
+      viewAreaSearchBox: false,
     };
   },
-  methods:{
-    searchUserMovies(){
-        this.$emit('searchMovies', this.searchMoviesText);
+  methods: {
+    searchUserMovies() {
+      this.$emit('searchMovies', this.searchMoviesText);
+    },
+    viewSearch() {
+      const areaSearchBox = document.querySelector(".searchMoviesAndSeries");
+
+      if (this.viewAreaSearchBox) {
+        this.viewAreaSearchBox = false;
+        console.log(this.viewAreaSearchBox);
+        areaSearchBox.classList.remove("view");
+      } else {
+        this.viewAreaSearchBox = true;
+        console.log(this.viewAreaSearchBox);
+        areaSearchBox.classList.add("view");
+      }
     }
   },
-  props:{
+  props: {
     listMoviesArr: Array
   }
 };
 </script>
 
 <style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@500&display=swap');
+
+header {
+  background-color: #141414;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 50px;
+
+  .logo {
+    width: 100px;
+    cursor: pointer;
+  }
+
+  .navigationBar {
+    flex-grow: 1;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 40px;
+    font-family: 'Red Hat Display', sans-serif;
+  }
+
+  .navDx {
+    position: relative;
+
+    .searchMoviesAndSeries {
+      width: 0px;
+      position: absolute;
+      display: flex;
+      align-items: center;
+      top: 50%;
+      transform: translateY(-15px);
+      right: 200px;
+      overflow: hidden;
+      transition: width 1s;
+
+      input {
+        width: 270px;
+        height: 30px;
+      }
+
+      button {
+        margin-left: 15px;
+        padding: 5px 15px;
+        background-color: #252525;
+        color: #fff;
+        border: 3px solid #3a3a3a;
+        border-radius: 5px;
+        cursor: pointer;
+
+        &:hover {
+          background-color: #373737;
+        }
+      }
+    }
+  }
+
+  .searchMoviesAndSeries.view {
+    width: 350px;
+  }
+
+  .navSx ul,
+  .navDx ul {
+    display: flex;
+    align-items: center;
+    color: #ccc;
+
+    li {
+      list-style-type: none;
+      margin: 0 12px;
+      cursor: pointer;
+
+      .active,
+      &:hover {
+        color: #fff;
+      }
+
+      i {
+        font-size: 1.3rem;
+        padding: 0 3px;
+      }
+
+      img {
+        width: 40px;
+        height: 40px;
+        border-radius: 50px;
+        cursor: default;
+      }
+    }
+  }
+}
 </style>
